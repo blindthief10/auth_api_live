48f38dfdc573c0d391bc3846e641ddbfbbd6d1ee
blindthief10 <kostasdiakogiannis23@gmail.com>
2019-05-15 10:39:33 +0200

    password hashed on creation

--- middleware/userMiddleware.js
+++ middleware/userMiddleware.js
@@ -1,5 +1,8 @@
 const usersModel = require('../models/usersModel');
 const { validationResult } = require('express-validator/check');
+const bcrypt = require('bcrypt');
+const dotenv = require('dotenv').config();
+const saltRounds = parseInt(process.env.SALT_ROUNDS);
 
 const createUser = async (req, res, next) => {
   try {
@@ -8,7 +11,9 @@ const createUser = async (req, res, next) => {
     if (!validationErrors.isEmpty()) {
       return res.status(400).json({errors: validationErrors.array()});
     }
-    
+
+    req.body.password = await bcrypt.hash(req.body.password, saltRounds);
+
     await usersModel.create(req.body);
     res.status(201).json({msg: 'User was created!'});
   }catch (error) {
